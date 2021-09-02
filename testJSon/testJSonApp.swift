474455346329be09cc9d57aac29017db5032edac
Stas <staszhingel@icloud.com>
2021-09-02 11:11:35 +0300

    Initial Commit

---
+++ testJSon/testJSonApp.swift
@@ -0,0 +1,17 @@
+//
+//  testJSonApp.swift
+//  testJSon
+//
+//  Created by Стас Жингель on 02.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct testJSonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
